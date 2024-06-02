63626c85a1b64070af64b56b8b83b0c8e7fb1a70
Anton Patsev <patsev.anton@gmail.com>
2024-06-02 13:24:57 +0600

    Check create_vpc in data yandex_vpc_network

--- examples/vpc/main.tf
+++ examples/vpc/main.tf
@@ -3,9 +3,14 @@ module "network" {
 
   blank_name = "testnetwork"
   labels     = var.labels
+  folder_id  = "xxx"
+
 
   azs = var.azs
 
   public_subnets  = var.subnets["public"]
   private_subnets = var.subnets["private"]
+
+  create_vpc = true
+  #vpc_id     = "xxx"  # установите значение вашего существующего VPC ID, если create_vpc = false
 }
