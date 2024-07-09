0da3dca5698b40108d8d37cd79dd18c5ef36d85d
Anton Patsev <patsev.anton@gmail.com>
2024-07-09 17:13:35 +0600

    Fix example

--- examples/vpc/main.tf
+++ examples/vpc/main.tf
@@ -1,9 +1,6 @@
 module "network" {
   source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-vpc.git"
 
-  public_subnets  = [["10.1.0.0/24"], ["10.2.0.0/24"], ["10.3.0.0/24"]]
-  private_subnets = [["10.4.0.0/24"], ["10.5.0.0/24"], ["10.6.0.0/24"]]
-
   blank_name = "testnetwork"
   labels = {
     testlabel = "testvalue"
@@ -11,6 +8,9 @@ module "network" {
   folder_id  = "xxxx"
 
   azs = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
+
+  public_subnets  = [["10.1.0.0/24"], ["10.2.0.0/24"], ["10.3.0.0/24"]]
+  private_subnets = [["10.4.0.0/24"], ["10.5.0.0/24"], ["10.6.0.0/24"]]
 
   create_vpc = true
   #vpc_id     = "xxx"  # установите значение вашего существующего VPC ID, если create_vpc = false
