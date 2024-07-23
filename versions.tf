e3cc590feac99519cc57137962ed092d8c95c570
Anton Patsev <patsev.anton@gmail.com>
2024-07-23 12:18:48 +0600

    Fix tflint required_providers

--- versions.tf
+++ versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.72.0"
     }
   }
   required_version = ">= 1.3"
